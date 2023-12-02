a41323bb1f1a8ef473606573be680b14e00d0c21
someonesdiary <60587698+someonesdiary@users.noreply.github.com>
2023-12-02 16:13:33 +0300

    02/12/2023@14:58

---
+++ under construction/script.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date("Mar 02, 2024 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("clock").innerHTML = "[ANY MINUTE NOW]";
+  }
+}, 1000);
